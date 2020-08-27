f3523eb41a116312783edd3763028f2e53850897
Pablo Asensio Martínez <44872361+pasensio97@users.noreply.github.com>
2020-08-27 04:14:21 +0200

    Add files via upload
    Fixed bugs. Now is possible to pause.

--- live_game.py
+++ live_game.py
@@ -1,7 +1,8 @@
 import pygame
 import numpy as np
+import time
 
-# setup display 
+# setup display
 pygame.init()
 HEIGHT, WIDTH = 800, 800
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -9,72 +10,67 @@ pygame.display.set_caption("Live game")
 # UPD = pygame.display.update()
 
 # rectangle dimansions
-# RWIDTH = int( min(WIDTH, HEIGHT) / 10 ) 
+# RWIDTH = int( min(WIDTH, HEIGHT) / 10 )
 # nyC = int(WIDTH/RWIDTH) # numero celdas eje y
 # nxC = int(HEIGHT/RWIDTH) # numero celdas eje x
 
-nyC, nxC = 25, 25
-dimX = WIDTH / nyC
-dimY = HEIGHT / nxC
+# set number rows and cols
+nyC, nxC = 20, 20
+dimY = WIDTH / nyC
+dimX = HEIGHT / nxC
 
 # colors
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-GREY = (25,25,25)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (25, 25, 25)
 win.fill(GREY)
 
 # Game State; 0 dead, 1 live
 global gameState
-gameState = np.zeros( (nxC, nyC) )
+gameState = np.zeros((nxC, nyC))
 
 # automata palo
-gameState[10,5] = 1
-gameState[10,6] = 1
-gameState[10,7] = 1
+gameState[5, 5] = 1
+gameState[5, 6] = 1
+gameState[5, 7] = 1
 
+gameState[12, 12] = 1
+gameState[13, 12] = 1
+gameState[14, 12] = 1
 
-# setup game loop
-FPS = 2
+# Setup game loop
+LPS = 1  # Loops per second
+FPS = 30
 clock = pygame.time.Clock()
 run = True
 
+# Game function
 def game(gameState):
-    new_gameState = np.copy( gameState )
+    new_gameState = np.copy(gameState)
     for y in range(0, nyC):
         for x in range(0, nxC):
 
             # se suman el numero de vecinos vivos
-            n_neight = gameState[ (x-1) % nxC ][ (y-1) % nyC ] + \
-                        gameState[ (x-1) % nxC ][ y % nyC ] + \
-                        gameState[ (x-1) % nxC ][ (y+1) % nyC ] + \
-                        gameState[ x % nxC ][ (y-1) % nyC ] + \
-                        gameState[ x % nxC ][ (y+1) % nyC ] + \
-                        gameState[ (x+1) % nxC ][ (y-1) % nyC ] + \
-                        gameState[ (x+1) % nxC ][ y % nyC ] + \
-                        gameState[ (x+1) % nxC ][ (y+1) % nyC ]
-            
-            # se aplica la norma del juego
-            if gameState[x,y] == 0 and n_neight == 3:
-                new_gameState[x,y] = 1
+            n_neight = (
+                gameState[(x - 1) % nxC, (y - 1) % nyC]
+                + gameState[(x - 1) % nxC, (y) % nyC]
+                + gameState[(x - 1) % nxC, (y + 1) % nyC]
+                + gameState[(x) % nxC, (y - 1) % nyC]
+                + gameState[(x) % nxC, (y + 1) % nyC]
+                + gameState[(x + 1) % nxC, (y - 1) % nyC]
+                + gameState[(x + 1) % nxC, (y) % nyC]
+                + gameState[(x + 1) % nxC, (y + 1) % nyC]
+            )
 
-            if gameState[x,y] == 1 and (n_neight < 2 or n_neight > 3):
-                new_gameState[x,y] = 0
+            # Game rules
+            if gameState[x, y] == 0 and n_neight == 3:
+                new_gameState[x, y] = 1
 
+            if gameState[x, y] == 1 and (n_neight < 2 or n_neight > 3):
+                new_gameState[x, y] = 0
 
-            # rect = [
-            #     (y) * RWIDTH,
-            #     (x) * RWIDTH,
-            #     (y+1) * RWIDTH,
-            #     (x+1) * RWIDTH
-            # ]
-
-
-            rect = [
-                (y) * dimX,
-                (x) * dimY,
-                (y+1) * dimX,
-                (x+1) * dimY
-            ]
+            # Drawing rect
+            rect = [(y) * dimY, (x) * dimX, dimY, dimX]
             if new_gameState[x][y] == 0:
                 pygame.draw.rect(win, BLACK, rect, 1)
             else:
@@ -82,15 +78,21 @@ def game(gameState):
 
     gameState = np.copy(new_gameState)
     return gameState
-        
-# print(nyC,nxC,RWIDTH)
+
+
+# Control vars
+pauseState = False
+time_now = time.clock()
 
 while run:
-    clock.tick(FPS)
+    # clock.tick(FPS)
     win.fill(GREY)
-    gameState = game(gameState)
 
-    pygame.display.update()
+    time2 = time.clock() - time_now
+    if time2 > 1.0 / LPS + 0.1 and not pauseState:
+        gameState = game(gameState)
+        pygame.display.update()
+        time_now = time.clock()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -98,5 +100,8 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             print(pos)
+        if event.type == pygame.KEYDOWN:
+            pauseState = not pauseState
+
 
 pygame.quit()
